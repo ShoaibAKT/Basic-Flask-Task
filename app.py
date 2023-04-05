d0a571a14d646a36401b2cbcc5fd5456f6f5cea9
Shoaib Ahmad Khan <129833350+ShoaibAKT@users.noreply.github.com>
2023-04-05 12:38:23 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
-from flask import Flask, request
-
+from flask import Flask, redirect, url_for, render_template, request
+import hashlib
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    name = request.args.get('name')
-    if name:
-        return f'Hello {name}!'
-    else:
-        return 'Please provide a name.'
+@app.route('/')
+def welcome():
+    return render_template('index.html')
 
-@app.route('/square')
-def square():
-    num = request.args.get('num')
-    if num:
-        try:
-            num = int(num)
-            result = num ** 2
-            return f'The square of {num} is {result}.'
-        except ValueError:
-            return 'Please provide a valid integer.'
-    else:
-        return 'Please provide a number.'
+
+@app.route('/submit', methods=['POST','GET'])
+def submit():
+    if request.method == 'POST':
+        img = request.form['Path']
+        with open(img, "rb") as f:
+            im_bytes = f.read()
+        im_hash = str(hashlib.md5(im_bytes).hexdigest())
+        return "<b>Here is the md5Hash of the Image :  </b>"+ im_hash
+        
+            
+
+if __name__ == '__main__':
+    app.run(debug=True)
